fcada23103bda4a221d84c6ebd15314259cf3275
Igor <igoribuzian@gmail.com>
2025-07-28 12:41:43 +0300

    Add Project

---
+++ Micro-Sercive/src/test/java/com/example/Micro_Sercive/MicroSerciveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Micro_Sercive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroSerciveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
